f4f6524e32e79e738051a1049499a34b4725a48c
Nishat1987 <nishat616901@gmail.com>
2023-06-30 02:12:48 -0500

    Prime Number Assignment

---
+++ src/june_23Selenium2/PrimeNumber.java
@@ -0,0 +1,21 @@
+package june_23Selenium2;
+
+public class PrimeNumber {
+public static void main(String[] args) {
+	   
+	int num = 12;
+	boolean isPrime = true;
+	
+	for(int i = 2; i< num; i++) {
+		if(num % i == 0)
+			{isPrime = false;
+		break;
+			}}
+
+		if(isPrime)
+			{System.out.println(num + " is a prime number");
+}else{
+	System.out.println(num + " is not a prime number");
+	}
+}}
+
--- src/june_23Selenium2/Test1.java
+++
@@ -1,7 +0,0 @@
-package june_23Selenium2;
-
-public class Test1 {
-public static void main(String[] args) {
-	
-}
-}
