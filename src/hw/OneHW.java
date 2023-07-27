eef6233aa279d1ea0a558dd42304ad3b0f8da85a
Nishat1987 <nishat616901@gmail.com>
2023-07-27 02:21:37 -0500

    HomeWork number1.

---
+++ src/hw/OneHW.java
@@ -0,0 +1,13 @@
+package hw;
+
+ class OneHW {
+	 private static int x;
+	public static void main(String[] args) {
+		
+		System.out.println(fun());
+	}
+		
+	static int fun() {
+			return ++x;
+		}
+	}
